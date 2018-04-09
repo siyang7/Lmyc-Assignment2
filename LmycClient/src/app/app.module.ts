2ab06a73635e9e026940b030c8769e7872b23664
Si-Yang <cameraman03@hotmail.com>
2018-04-08 23:52:33 -0700

    a

--- LmycClient/src/app/app.module.ts
+++ LmycClient/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { GetBoatsComponent } from './get-boats/get-boats.component';
 import { GetReservationsComponent } from './get-reservations/get-reservations.component';
 import { LoginComponent } from './login/login.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,12 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'get-boats', component: GetBoatsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
